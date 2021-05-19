83684f522c50d59c46780599afe5df18b8bb71b5
Savu Sebastian <savusebastianf@gmail.com>
2021-05-19 21:22:17 +0300

    a

--- schoolpointe.py
+++ schoolpointe.py
@@ -123,8 +123,8 @@ def get_content(web_page, splitter):
 	issue_pages_counter = 0
 	print(web_page)
 
-	if web_page != '#':
-	# try:
+	# if web_page != '#':
+	try:
 		web_link = requests.get(web_page, timeout=10).content
 		web_soup = BeautifulSoup(web_link, 'html.parser')
 
@@ -155,7 +155,7 @@ def get_content(web_page, splitter):
 		if web_soup.find(class_='maincontentsection').find_all(id='news-list') != []:
 			news = 'news'
 
-		if web_soup.find_all(class_='hidden-xs col-sm-3')[1] != None:
+		if len(web_soup.find_all(class_='hidden-xs col-sm-3')) > 1:
 			page_nav = web_soup.find_all(class_='hidden-xs col-sm-3')[1].find_all('a')
 
 		# First column
@@ -205,8 +205,8 @@ def get_content(web_page, splitter):
 
 		return col1, col2, col3, col4, col_num, page_nav, meta_title, meta_keywords, meta_desc, form, embed, iframe, calendar, staff, news, issue_pages_counter
 
-	else:
-	# except:
+	# else:
+	except:
 		issue_pages_counter = 1
 
 		return col1, col2, col3, col4, col_num, page_nav, meta_title, meta_keywords, meta_desc, form, embed, iframe, calendar, staff, news, issue_pages_counter

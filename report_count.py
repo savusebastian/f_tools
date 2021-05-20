8b43c7a0704172a70f2013aa4b4f723bcaabbb45
Savu Sebastian <savusebastianf@gmail.com>
2021-05-20 12:47:39 +0300

    a

--- report_count.py
+++ report_count.py
@@ -12,15 +12,18 @@ while True:
 	except OverflowError:
 		max_int = int(max_int / 10)
 
-folder = '../f_web_interface/static/files/buckeyevalley/'
+folder = '../f_web_interface/static/files/masoncoschools/'
 files = glob.glob(f'{folder}*.csv')
 # print(files)
 files.remove(f'{folder}report.csv')
 pages = 0
 issues = 0
-t1 = []
-t2 = []
-t3 = []
+t1 = 0
+t2 = 0
+t3 = 0
+t1_arr = []
+t2_arr = []
+t3_arr = []
 
 for file in files:
 	with open(file, 'r', encoding='utf-8') as csv_file:
@@ -30,21 +33,25 @@ for file in files:
 			pages += 1
 
 			if len(row) > 0 :
-				if not row[1] in t1:
-					t1.append(row[1])
+				if not row[1] in t1_arr:
+					t1_arr.append(row[1])
 
-				if not row[2] in t2 and len(row[2]) > 0:
-					t2.append(row[2])
+				if not row[2] in t2_arr and len(row[2]) > 0:
+					t2_arr.append(row[2])
 
-				if not row[3] in t3 and len(row[3]) > 0:
-					t3.append(row[3])
+				if not row[3] in t3_arr and len(row[3]) > 0:
+					t3_arr.append(row[3])
 
 				if not row[6] in t3 and row[6] == 'Flagged':
 					issues += 1
 
+	t1 += len(t1_arr)
+	t2 += len(t2_arr)
+	t3 += len(t3_arr)
+
 
 print('Pages:', pages - len(files))
 print('Issues:', issues)
-print('T1:', len(t1))
-print('T2:', len(t2))
-print('T3:', len(t3))
+print('T1:', t1)
+print('T2:', t2)
+print('T3:', t3)

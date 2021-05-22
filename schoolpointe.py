63cd3486a5ccb97b1359bb4c5ef8978b002a0e43
Savu Sebastian <savusebastianf@gmail.com>
2021-05-22 15:36:04 +0300

    a

--- schoolpointe.py
+++ schoolpointe.py
@@ -259,7 +259,7 @@ if __name__ == '__main__':
 			page = requests.get(site, headers=headers, timeout=5).content
 			soup = BeautifulSoup(page, 'html.parser')
 			sitemap = soup.find(id='bs-example-navbar-collapse-1')
-			print(soup)
+			
 			list_items = sitemap.select('ul > li')
 			# list_items1 = sitemap.select('ul > li')
 			# sitemap2 = soup.find(class_='header-elements')

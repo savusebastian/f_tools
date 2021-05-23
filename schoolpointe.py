5c21c43a5e0a7f6717b789266cef0a3ef2f2b977
Savu Sebastian <savusebastianf@gmail.com>
2021-05-23 12:27:03 +0300

    a

--- schoolpointe.py
+++ schoolpointe.py
@@ -148,22 +148,22 @@ def get_content(web_page):
 		if web_soup.find_all('meta', attrs={'name': 'description'}) != []:
 			meta_desc = str(web_soup.find_all('meta', attrs={'name': 'description'}))
 
-		if web_soup.find(class_='body-content-container').find_all('form') != []:
+		if web_soup.find(id='site-body').find_all('form') != []:
 			form = 'form'
 
-		if web_soup.find(class_='body-content-container').find_all('embed') != []:
+		if web_soup.find(id='site-body').find_all('embed') != []:
 			embed = 'embed'
 
-		if web_soup.find(class_='body-content-container').find_all('iframe') != []:
+		if web_soup.find(id='site-body').find_all('iframe') != []:
 			iframe = 'iframe'
 
-		if web_soup.find(class_='body-content-container').find_all(id='calendar') != []:
+		if web_soup.find(id='site-body').find_all(id='calendar') != []:
 			calendar = 'calendar'
 
-		if web_soup.find(class_='body-content-container').find_all(class_='staff-directory') != []:
+		if web_soup.find(id='site-body').find_all(class_='staff-directory') != []:
 			staff = 'staff'
 
-		if web_soup.find(class_='body-content-container').find_all(id='news-list') != []:
+		if web_soup.find(id='site-body').find_all(id='news-list') != []:
 			news = 'news'
 
 		# if web_soup.find(class_='hidden-xs show-on-olc col-sm-4 col-md-3 col-lg-3 backgroundcolor leftColumn') != None:
@@ -172,8 +172,8 @@ def get_content(web_page):
 		# 	page_nav = web_soup.find(id='quicklinks').find_all('a')
 
 		# Content
-		if web_soup.find(class_='body-content-container') != None and web_soup.find(class_='body-content-container') != '':
-			col1 = web_soup.find(class_='body-content-container')
+		if web_soup.find(id='site-body') != None and web_soup.find(id='site-body') != '':
+			col1 = web_soup.find(id='site-body')
 			col1 = get_column(col1)
 		else:
 			issue_pages_counter = 1
@@ -226,14 +226,14 @@ def get_content(web_page):
 if __name__ == '__main__':
 	start_time = time()
 	all_sites = [
-		'https://www.woodscharter.org',
-		# 'https://www.woodscharter.org/6/Home',
-		# 'https://www.woodscharter.org/8/Home',
-		# 'https://www.woodscharter.org/4/Home',
-		# 'https://www.woodscharter.org/5/Home',
-		# 'https://www.woodscharter.org/1/Home',
-		# 'https://www.woodscharter.org/7/home',
-		# 'https://www.woodscharter.org/2/Home',
+		'https://www.ksb.kyschools.us',
+		# 'https://www.ksb.kyschools.us/6/Home',
+		# 'https://www.ksb.kyschools.us/8/Home',
+		# 'https://www.ksb.kyschools.us/4/Home',
+		# 'https://www.ksb.kyschools.us/5/Home',
+		# 'https://www.ksb.kyschools.us/1/Home',
+		# 'https://www.ksb.kyschools.us/7/home',
+		# 'https://www.ksb.kyschools.us/2/Home',
 	]
 	schools = [
 		'district',
@@ -264,18 +264,18 @@ if __name__ == '__main__':
 
 			page = requests.get(site, timeout=5).content
 			soup = BeautifulSoup(page, 'html.parser')
-			sitemap = soup.find(class_='top-nav-wrapper')
+			sitemap = soup.find(id='MainNav')
 			# list_items = sitemap.select('ul > li')
 			list_items1 = sitemap.select('ul > li')
 
-			sitemap2 = soup.find(class_='quicklinkscolumn')
-			list_items2 = sitemap2.select('ul.quicklinks > li')
+			sitemap2 = soup.find(id='HomeCircleNav')
+			list_items2 = sitemap2.select('ul > li')
 
-			sitemap3 = soup.find(id='bs-example-navbar-collapse-1')
-			list_items3 = sitemap3.select('ul.very-top-nav > li')
+			# sitemap3 = soup.find(id='bs-example-navbar-collapse-1')
+			# list_items3 = sitemap3.select('ul.very-top-nav > li')
 
-			# list_items = itertools.chain(list_items1, list_items2)
-			list_items = itertools.chain(list_items1, list_items2, list_items3)
+			list_items = itertools.chain(list_items1, list_items2)
+			# list_items = itertools.chain(list_items1, list_items2, list_items3)
 
 			school_name = f'{split_dot[1]}_{schools[s - 1]}'
 			csv_report.writerow(['School name', school_name])

50fca85463010eab6e17be1c0d9fc4305e7403cf
Savu Sebastian <savusebastianf@gmail.com>
2021-05-25 14:42:57 +0300

    a

--- schoolpointe.py
+++ schoolpointe.py
@@ -151,22 +151,22 @@ def get_content(web_page):
 		if web_soup.find_all('meta', attrs={'name': 'description'}) != []:
 			meta_desc = str(web_soup.find_all('meta', attrs={'name': 'description'}))
 
-		if web_soup.find(class_='masterC alignment-stretch').find_all('form') != []:
+		if web_soup.find(class_='main-content').find_all('form') != []:
 			form = 'form'
 
-		if web_soup.find(class_='masterC alignment-stretch').find_all('embed') != []:
+		if web_soup.find(class_='main-content').find_all('embed') != []:
 			embed = 'embed'
 
-		if web_soup.find(class_='masterC alignment-stretch').find_all('iframe') != []:
+		if web_soup.find(class_='main-content').find_all('iframe') != []:
 			iframe = 'iframe'
 
-		if web_soup.find(class_='masterC alignment-stretch').find_all(id='calendar') != []:
+		if web_soup.find(class_='main-content').find_all(id='calendar') != []:
 			calendar = 'calendar'
 
-		if web_soup.find(class_='masterC alignment-stretch').find_all(class_='staff-directory') != []:
+		if web_soup.find(class_='main-content').find_all(class_='staff-directory') != []:
 			staff = 'staff'
 
-		if web_soup.find(class_='masterC alignment-stretch').find_all(id='news-list') != []:
+		if web_soup.find(class_='main-content').find_all(id='news-list') != []:
 			news = 'news'
 
 		# if web_soup.find(class_='hidden-xs show-on-olc col-sm-4 col-md-3 col-lg-3 backgroundcolor leftColumn') != None:
@@ -175,8 +175,8 @@ def get_content(web_page):
 		# 	page_nav = web_soup.find(id='quicklinks').find_all('a')
 
 		# Content
-		if web_soup.find(class_='masterC alignment-stretch') != None and web_soup.find(class_='masterC alignment-stretch') != '':
-			col1 = web_soup.find(class_='masterC alignment-stretch')
+		if web_soup.find(class_='main-content') != None and web_soup.find(class_='main-content') != '':
+			col1 = web_soup.find(class_='main-content')
 			col1 = get_column(col1)
 		else:
 			issue_pages_counter = 1
@@ -228,20 +228,20 @@ def get_content(web_page):
 
 if __name__ == '__main__':
 	start_time = time()
-	district = 'http://www.sidney.k12.oh.us.schools.bz'
+	district = 'http://www.reverek12.org'
 	all_sites = [
 		f'{district}',
+		f'{district}/6/home',
+		f'{district}/10/home',
+		f'{district}/7/home',
+		f'{district}/8/home',
+		f'{district}/4/home',
+		f'{district}/3/home',
+		f'{district}/12/home',
 		f'{district}/1/home',
 		f'{district}/2/home',
-		f'{district}/5/home',
-		f'{district}/3/home',
-		f'{district}/4/home',
 		f'{district}/9/home',
-		# f'{district}/7/home',
-		# f'{district}/6/home',
-		# f'{district}/8/home',
-		# f'{district}/9/home',
-		# f'{district}/3/home',
+		f'{district}/5/home',
 		# f'{district}/12/home',
 		# f'{district}/14/home',
 		# f'{district}/15/home',
@@ -254,17 +254,17 @@ if __name__ == '__main__':
 	]
 	schools = [
 		'district',
+		'acwes',
+		'als',
+		'bvms',
+		'ges',
+		'gms',
+		'pris',
+		'rhs',
+		'rma',
 		'shs',
-		'sms',
-		'nis',
-		'eps',
-		'lps',
-		'wecc',
-		# 'lcatc',
-		# 'oes',
-		# 'pces',
-		# 'wes',
-		# 'ses',
+		'ssjjhes',
+		'sbams',
 		# 'kes',
 		# 'ms',
 		# 'mullinsschool',

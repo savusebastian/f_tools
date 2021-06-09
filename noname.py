515d784764c4eb6de4af54254fce3ea86069083e
Savu Sebastian <savusebastianf@gmail.com>
2021-06-09 21:44:34 +0300

    a

--- noname.py
+++ noname.py
@@ -312,24 +312,28 @@ if __name__ == '__main__':
 
 				page_link = link
 
-				page_counter += 1
-				col1, col2, col3, col4, col_num, nav_sec, meta_title, meta_keywords, meta_desc, form, embed, iframe, calendar, staff, news, content_ipc = get_content(page_link)
-				issue_pages_counter += content_ipc
+				if href.find('.pdf') > -1 or href.find('.mp3') > -1 or href.find('.wmv') > -1 or href.find('.mp4') > -1 or href.find('.docx') > -1 or href.find('.xlsx') > -1 or href.find('.pptx') > -1\
+				or href.find('.doc') > -1 or href.find('.xls') > -1 or href.find('.ppt') > -1 or href.find('.jsp') > -1 or href.find('.m4v') > -1 or href.find('.mkv') > -1:
+					csv_writer.writerow([str(page_link), t1, t2, '', '', '', '', '1', 'Linked file', '', '', '', '', '', ''])
+				else:
+					page_counter += 1
+					col1, col2, col3, col4, col_num, nav_sec, meta_title, meta_keywords, meta_desc, form, embed, iframe, calendar, staff, news, content_ipc = get_content(page_link)
+					issue_pages_counter += content_ipc
 
-				csv_writer.writerow([str(page_link), t1, t2, t3, t4, t5, t6, col_num, col1, col2, col3, col4, meta_title, meta_keywords, meta_desc])
+					csv_writer.writerow([str(page_link), t1, t2, t3, t4, t5, t6, col_num, col1, col2, col3, col4, meta_title, meta_keywords, meta_desc])
 
-				if form != '' or embed != '' or iframe != '' or calendar != '' or staff != '' or news != '':
-					csv_report.writerow([str(page_link), form, embed, iframe, calendar, staff, news])
+					if form != '' or embed != '' or iframe != '' or calendar != '' or staff != '' or news != '':
+						csv_report.writerow([str(page_link), form, embed, iframe, calendar, staff, news])
 
-				# if nav_sec != None and nav_sec != '' and nav_sec != []:
-				# 	for nav_link in nav_sec:				#
-				# 		page_counter += 1
-				# 		nav_col1, nav_col2, nav_col3, nav_col4, nav_col_num, _, meta_title, meta_keywords, meta_desc, form, embed, iframe, calendar, staff, news, content_ipc = get_content(page_link)
-				# 		issue_pages_counter += content_ipc
-				# 		csv_writer.writerow([str(page_link), str(group_links[0].get_text()), str(link.get_text()), str(nav_link.get_text()), '', nav_col_num, nav_col1, nav_col2, nav_col3, nav_col4, meta_title, meta_keywords, meta_desc])
-				#
-				# 		if form != '' or embed != '' or iframe != '' or calendar != '' or staff != '' or news != '':
-				# 			csv_report.writerow([str(page_link), form, embed, iframe, calendar, staff, news])
+					# if nav_sec != None and nav_sec != '' and nav_sec != []:
+					# 	for nav_link in nav_sec:				#
+					# 		page_counter += 1
+					# 		nav_col1, nav_col2, nav_col3, nav_col4, nav_col_num, _, meta_title, meta_keywords, meta_desc, form, embed, iframe, calendar, staff, news, content_ipc = get_content(page_link)
+					# 		issue_pages_counter += content_ipc
+					# 		csv_writer.writerow([str(page_link), str(group_links[0].get_text()), str(link.get_text()), str(nav_link.get_text()), '', nav_col_num, nav_col1, nav_col2, nav_col3, nav_col4, meta_title, meta_keywords, meta_desc])
+					#
+					# 		if form != '' or embed != '' or iframe != '' or calendar != '' or staff != '' or news != '':
+					# 			csv_report.writerow([str(page_link), form, embed, iframe, calendar, staff, news])
 
 			csv_report.writerow([])
 			csv_report.writerow(['Pages scraped', page_counter])

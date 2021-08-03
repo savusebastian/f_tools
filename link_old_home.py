054077923f21ea5b891524101882fb6a664a2ace
Savu Sebastian <savusebastianf@gmail.com>
2021-08-03 11:06:08 +0300

    a

--- link_old_home.py
+++ link_old_home.py
@@ -23,19 +23,43 @@ def check_by_class(web_page):
 
 
 if __name__ == '__main__':
-	url = 'https://skschoolsorg.finalsite.com/fs/pages/sitemap.xml'
-	page = requests.get(url)
-	soup = BeautifulSoup(page.content, 'html.parser')
-	urls = soup.find_all('loc')
-	links = []
-	counter = 0
+	all_schools = [
+		'https://skschoolsorg.finalsite.com/site-map',
+		'https://skschoolsorg-23-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-42-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-49-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-47-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-48-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-50-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-51-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-52-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-53-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-54-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-56-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-55-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-57-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-58-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-59-us-west1-01.preview.finalsitecdn.com/site-map',
+		'https://skschoolsorg-60-us-west1-01.preview.finalsitecdn.com/site-map',
+	]
 
-	for link in urls:
-		if link.get_text()[0] != 'h':
-			d = check_by_class('https:' + link.get_text())
-		else:
-			d = check_by_class(link.get_text())
+	for url in all_schools:
+		page = requests.get(url)
+		soup = BeautifulSoup(page.content, 'html.parser')
+		urls = soup.find('main').find_all('a')
+		counter = 0
 
-		if d != []:
-			print('Link:', link.get_text())
-			print('Text of links:', d)
+		for link in urls:
+			if link.get_text()[0] != 'h':
+				d = check_by_class('https:' + link.get_text())
+			else:
+				d = check_by_class(link.get_text())
+
+			if d != []:
+				print('Link:', link.get_text())
+				print('Text of links:', d)
+
+		print()
+		print()
+		print()
+		print('Next school')

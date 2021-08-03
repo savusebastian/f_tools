d7b756c6b3c89ca084cef91e4eb0498cd1a49995
Savu Sebastian <savusebastianf@gmail.com>
2021-08-03 10:28:07 +0300

    a

--- link_old_home.py
+++ link_old_home.py
@@ -4,38 +4,25 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def check_news(web_page):
+def check_by_class(web_page):
 	try:
 		web_link = requests.get(web_page)
 		web_soup = BeautifulSoup(web_link.content, 'html.parser')
-		news = web_soup.find(class_='fsNews')
+		links = web_soup.find('main').find_all('a')
+		o = []
 
-		return news != None
+		for link in links:
+			if link.get('href') != None and len(link.get('href')) > 19 and (link.get('href')[:25] == 'https://www.skschools.org' or link.get('href')[:24] == 'http://www.skschools.org' or link.get('href')[:21] == 'https://skschools.org' or link.get('href')[:20] == 'http://skschools.org'):
+				o.append(link.get_text())
 
-	except:
+		return o
+
+	except Exception:
 		pass
+		# print('Page not working:', web_page)
 
 
 if __name__ == '__main__':
-	def check_by_class(web_page):
-		try:
-			web_link = requests.get(web_page)
-			print(web_page)
-			web_soup = BeautifulSoup(web_link.content, 'html.parser')
-			links = web_soup.find('main').find_all('a')
-			o = []
-
-			for link in links:
-				if link.get('href') != None and len(link.get('href')) > 19 and (link.get('href')[:25] == 'https://www.skschools.org' or link.get('href')[:24] == 'http://www.skschools.org' or link.get('href')[:21] == 'https://skschools.org' or link.get('href')[:20] == 'http://skschools.org'):
-					o.append(link.get_text())
-
-			return o
-
-		except:
-			pass
-			# print('Page not working:', web_page)
-
-
 	url = 'https://skschoolsorg.finalsite.com/fs/pages/sitemap.xml'
 	page = requests.get(url)
 	soup = BeautifulSoup(page.content, 'html.parser')
@@ -50,5 +37,5 @@ if __name__ == '__main__':
 			d = check_by_class(link.get_text())
 
 		if d != []:
-			print('>', link)
-			print(d)
+			print('Link:', link.get_text())
+			print('Text of links:', d)

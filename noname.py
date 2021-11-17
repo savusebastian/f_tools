27e4c76be59d9a81ff9b8c158f650c32110301b2
Savu Sebastian <savusebastianf@gmail.com>
2021-11-17 15:10:47 +0200

    a

--- noname.py
+++ noname.py
@@ -159,7 +159,7 @@ def get_content(web_page):
 	# try:
 		# headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/80.0'}
 		headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'}
-		web_link = requests.get(web_page, headers=headers, timeout=20, config={'danger_mode': True}, verify=False).content
+		web_link = requests.get(web_page, headers=headers, timeout=20, verify=False).content
 		web_soup = BeautifulSoup(web_link, 'html.parser')
 
 		if web_soup.find_all('meta', attrs={'name': 'title'}) != []:

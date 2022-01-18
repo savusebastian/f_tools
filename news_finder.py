b70beae9bd4c0c87e1ef9b8359fcaa0df396f389
Savu Sebastian <savusebastianf@gmail.com>
2022-01-18 10:58:42 +0200

    a

--- news_finder.py
+++ news_finder.py
@@ -18,29 +18,31 @@ def check_news(web_page):
 
 
 if __name__ == '__main__':
-	with open('all_sites.csv', 'r') as csv_file, open('news_finder.csv', 'w') as csv_main:
+	with open('all_sites.csv') as csv_file, open('news_finder.csv', 'w', encoding='utf-8') as csv_main:
 		csv_reader = csv.reader(csv_file)
 		csv_writer = csv.writer(csv_main)
 		# csv_writer.writerow(csv_reader[0])
+		for row in csv_reader:
+			print(row)
 
-		for row in csv_reader[1:]:
-			URL = row[0]
-			print(URL)
-			news_links = ''
-			sitemap = requests.get(f'{URL}fs/pages/sitemap.xml').content
-			soup = BeautifulSoup(sitemap, 'html.parser')
-			links = soup.find_all('loc')
-
-			for link in links:
-				if link.get_text()[0] == '/':
-					news = check_news(f'https:{link.get_text()}')
-				else:
-					news = check_news(link.get_text())
-
-				if news:
-					news_links += f'{link.get_text()}\n'
-
-			if news_links == '':
-				news_links = 'No news found'
-
-			csv_writer.writerow([URL, news_links])
+		# for row in csv_reader[1:]:
+		# 	URL = row[0]
+		# 	print(URL)
+		# 	news_links = ''
+		# 	sitemap = requests.get(f'{URL}fs/pages/sitemap.xml').content
+		# 	soup = BeautifulSoup(sitemap, 'html.parser')
+		# 	links = soup.find_all('loc')
+		#
+		# 	for link in links:
+		# 		if link.get_text()[0] == '/':
+		# 			news = check_news(f'https:{link.get_text()}')
+		# 		else:
+		# 			news = check_news(link.get_text())
+		#
+		# 		if news:
+		# 			news_links += f'{link.get_text()}\n'
+		#
+		# 	if news_links == '':
+		# 		news_links = 'No news found'
+		#
+		# 	csv_writer.writerow([URL, news_links])

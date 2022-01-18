61aa888c89886eee45a795f3b755380f34529eba
Savu Sebastian <savusebastianf@gmail.com>
2022-01-18 11:26:23 +0200

    a

--- news_finder.py
+++ news_finder.py
@@ -21,28 +21,28 @@ if __name__ == '__main__':
 	with open('all_sites.csv') as csv_file, open('news_finder.csv', 'w', encoding='utf-8') as csv_main:
 		csv_reader = csv.reader(csv_file)
 		csv_writer = csv.writer(csv_main)
-		x=next(csv_reader)
-		print(x)
-		csv_writer.writerow(x)
+		csv_writer.writerow(next(csv_reader))
 
 		for row in csv_reader:
-			URL = row[0]
-			print(URL)
+			col_0 = row[0]
+			print(col_0)
 			news_links = ''
-			sitemap = requests.get(f'{URL}fs/pages/sitemap.xml').content
+			sitemap = requests.get(f'{col_0}fs/pages/sitemap.xml').content
 			soup = BeautifulSoup(sitemap, 'html.parser')
 			links = soup.find_all('loc')
 
 			for link in links:
-				if link.get_text()[0] == '/':
-					news = check_news(f'https:{link.get_text()}')
+				text = link.get_text()
+
+				if text[0] == '/':
+					news = check_news(f'https:{text}')
 				else:
-					news = check_news(link.get_text())
+					news = check_news(text)
 
 				if news:
-					news_links += f'{link.get_text()}\n'
+					news_links += f'{text}\n'
 
 			if news_links == '':
 				news_links = 'No news found'
 
-			csv_writer.writerow([URL, news_links])
+			csv_writer.writerow([col_0, news_links])

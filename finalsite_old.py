fe846d0ac630977662e248c004356ece3971e2fe
Sebastian Savu <sebi.savu@finalsite.com>
2022-01-19 10:31:16 +0200

    a

--- finalsite_old.py
+++ finalsite_old.py
@@ -275,7 +275,7 @@ if __name__ == '__main__':
 			split_mixed = site.split('/')[2].split('.')
 			all_links = []
 
-			page = requests.get(site, headers={'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'}).content
+			page = requests.get(site, headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0'}, verify=False).content
 			soup = BeautifulSoup(page, 'html.parser')
 			sitemap = soup.find(id='content10')
 			list_items = sitemap.select('ul > li')

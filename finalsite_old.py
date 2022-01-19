06d78e7a7749f02efc785c441d86b6bf20f4640c
Sebastian Savu <sebi.savu@finalsite.com>
2022-01-19 10:10:10 +0200

    a

--- finalsite_old.py
+++ finalsite_old.py
@@ -160,7 +160,7 @@ def get_content(web_page):
 	# if web_page != '#':
 	try:
 		headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0'}
-		web_link = requests.get(web_page, headers=headers, timeout=5).content
+		web_link = requests.get(web_page, headers=headers, timeout=5, verify=False).content
 		web_soup = BeautifulSoup(web_link, 'html.parser')
 
 		if web_soup.find_all('meta', attrs={'name': 'title'}) != []:

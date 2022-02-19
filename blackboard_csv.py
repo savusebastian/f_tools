8029ccc9c6a318c8b9f3a844495c997accbbad19
Savu Sebastian <savusebastianf@gmail.com>
2022-02-19 09:46:54 +0200

    a

--- blackboard_csv.py
+++ blackboard_csv.py
@@ -127,9 +127,9 @@ if __name__ == '__main__':
 		# 'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1032&DirectoryType=6&SectionMax=50',
 		# 'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1145&DirectoryType=6&SectionMax=50',
 		# 'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1381&DirectoryType=6&SectionMax=50',
-		'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1492&DirectoryType=6&SectionMax=50',
-		'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1607&DirectoryType=6&SectionMax=50',
-		'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1722&DirectoryType=6&SectionMax=50',
+		# 'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1492&DirectoryType=6&SectionMax=50',
+		# 'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1607&DirectoryType=6&SectionMax=50',
+		# 'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1722&DirectoryType=6&SectionMax=50',
 		'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=1804&DirectoryType=6&SectionMax=50',
 		'https://www.barrington220.org/site/default.aspx?pagetype=15&SiteID=4166&DirectoryType=6&SectionMax=50',
 	]

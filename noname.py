47d9939c42240acf28e1d2a7c46a0000ae17236d
Savu Sebastian <savusebastianf@gmail.com>
2022-03-08 06:12:05 -0800

    a

--- noname.py
+++ noname.py
@@ -41,22 +41,22 @@ def get_content(web_page):
 		if web_soup.find_all('meta', attrs={'name': 'description'}) != []:
 			meta_desc = str(web_soup.find_all('meta', attrs={'name': 'description'}))
 
-		if web_soup.find(class_='pages-left-column-wrapper').find_all('form') != []:
+		if web_soup.find(class_='mainText').find_all('form') != []:
 			form = 'form'
 
-		if web_soup.find(class_='pages-left-column-wrapper').find_all('embed') != []:
+		if web_soup.find(class_='mainText').find_all('embed') != []:
 			embed = 'embed'
 
-		if web_soup.find(class_='pages-left-column-wrapper').find_all('iframe') != []:
+		if web_soup.find(class_='mainText').find_all('iframe') != []:
 			iframe = 'iframe'
 
-		if web_soup.find(class_='pages-left-column-wrapper').find_all(class_='calendar') != []:
+		if web_soup.find(class_='mainText').find_all(class_='calendar') != []:
 			calendar = 'calendar'
 
-		if web_soup.find(class_='pages-left-column-wrapper').find_all(class_='staff-directory') != []:
+		if web_soup.find(class_='mainText').find_all(class_='staff-directory') != []:
 			staff = 'staff'
 
-		if web_soup.find(class_='pages-left-column-wrapper').find_all(class_='news') != []:
+		if web_soup.find(class_='mainText').find_all(class_='news') != []:
 			news = 'news'
 
 		# if web_soup.find(class_='hidden-xs show-on-olc col-sm-4 col-md-3 col-lg-3 backgroundcolor leftColumn') != None:
@@ -65,8 +65,8 @@ def get_content(web_page):
 		# 	page_nav = web_soup.find(id='quicklinks').find_all('a')
 
 		# Content
-		if web_soup.find(class_='pages-left-column-wrapper') != None and web_soup.find(class_='pages-left-column-wrapper') != '':
-			col1 = web_soup.find(class_='pages-left-column-wrapper')
+		if web_soup.find(class_='mainText') != None and web_soup.find(class_='mainText') != '':
+			col1 = web_soup.find(class_='mainText')
 			col1 = get_column(col1)
 		else:
 			issue_pages_counter = 1

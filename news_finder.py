df69971ad43ce20bc1dc082b381c87afcd5a61cd
Savu Sebastian <savusebastianf@gmail.com>
2022-03-25 13:37:19 +0200

    a

--- news_finder.py
+++ news_finder.py
@@ -18,14 +18,15 @@ def check_news(web_page):
 
 
 if __name__ == '__main__':
-	with open('all_sites.csv') as csv_file, open('news_finder.csv', 'w', encoding='utf-8') as csv_main:
-		csv_reader = csv.reader(csv_file)
-		csv_writer = csv.writer(csv_main)
-		csv_writer.writerow(next(csv_reader))
-
-		for row in csv_reader:
-			col_0 = row[0]
-			print(col_0)
+	# with open('all_sites.csv') as csv_file, open('news_finder.csv', 'w', encoding='utf-8') as csv_main:
+	# 	csv_reader = csv.reader(csv_file)
+	# 	csv_writer = csv.writer(csv_main)
+	# 	csv_writer.writerow(next(csv_reader))
+	#
+	# 	for row in csv_reader:
+	# 		col_0 = row[0]
+			col_0 = 'https://www.garlandisdschools.net/'
+			# print(col_0)
 			news_links = ''
 			sitemap = requests.get(f'{col_0}fs/pages/sitemap.xml').content
 			soup = BeautifulSoup(sitemap, 'html.parser')

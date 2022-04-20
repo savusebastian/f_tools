1d50bd80c1f5378df5d40eef3ff78953c7149656
Savu Sebastian <83900114+savusebastian@users.noreply.github.com>
2022-04-20 10:19:52 +0300

    Update schoolpointe.py

--- schoolpointe.py
+++ schoolpointe.py
@@ -39,22 +39,22 @@ def get_content(web_page):
 		if web_soup.find_all('meta', attrs={'name': 'description'}) != []:
 			meta_desc = str(web_soup.find_all('meta', attrs={'name': 'description'}))
 
-		if web_soup.find(class_='maincontentsection').find_all('form') != []:
+		if web_soup.find(class_='main-content').find_all('form') != []:
 			form = 'form'
 
-		if web_soup.find(class_='maincontentsection').find_all('embed') != []:
+		if web_soup.find(class_='main-content').find_all('embed') != []:
 			embed = 'embed'
 
-		if web_soup.find(class_='maincontentsection').find_all('iframe') != []:
+		if web_soup.find(class_='main-content').find_all('iframe') != []:
 			iframe = 'iframe'
 
-		if web_soup.find(class_='maincontentsection').find_all(id='calendar') != []:
+		if web_soup.find(class_='main-content').find_all(id='calendar') != []:
 			calendar = 'calendar'
 
-		if web_soup.find(class_='maincontentsection').find_all(class_='staff-directory') != []:
+		if web_soup.find(class_='main-content').find_all(class_='staff-directory') != []:
 			staff = 'staff'
 
-		if web_soup.find(class_='maincontentsection').find_all(id='news-list') != []:
+		if web_soup.find(class_='main-content').find_all(id='news-list') != []:
 			news = 'news'
 
 		# if web_soup.find(class_='hidden-xs show-on-olc col-sm-4 col-md-3 col-lg-3 backgroundcolor leftColumn') != None:
@@ -63,11 +63,8 @@ def get_content(web_page):
 		# 	page_nav = web_soup.find(id='quicklinks').find_all('a')
 
 		# Content
-		if web_soup.find(class_='maincontentsection') != None and web_soup.find(class_='maincontentsection') != '':
-			col1 = web_soup.find(class_='maincontentsection')
-			col1 = get_column(col1)
-		elif web_soup.find(class_='content') != None and web_soup.find(class_='content') != '':
-			col1 = web_soup.find(class_='content')
+		if web_soup.find(class_='main-content') != None and web_soup.find(class_='main-content') != '':
+			col1 = web_soup.find(class_='main-content')
 			col1 = get_column(col1)
 		else:
 			issue_pages_counter = 1

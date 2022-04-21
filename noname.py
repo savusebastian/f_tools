b026e453b2e1c37db2e7569f6e03812840ceb79f
Savu Sebastian <savusebastianf@gmail.com>
2022-04-21 11:46:32 +0300

    a

--- noname.py
+++ noname.py
@@ -119,30 +119,24 @@ def get_content(web_page):
 if __name__ == '__main__':
 	start_time = time()
 	all_sites = [
-		'https://www.stamfordpublicschools.org/district/public-affairs/files/dolan-middle-school-2021',
-'https://docs.google.com/document/d/1ZGgP0eGwdA8fQoHKOEufcumNF4pYl1NY3lJjrbUW3t8/edit',
-'https://www.stamfordpublicschools.org/dolan-middle-school/information/files/bell-schedule',
-'https://www.stamfordpublicschools.org/dolan-middle-school/news/daily-rotation-calendar',
-'https://www.stamfordpublicschools.org/dolan-middle-school/news/dolan-bus-routes',
-'https://www.stamfordpublicschools.org/dolan-middle-school/information/files/dolan-school-strategic-improvement-plan',
-'https://www.stamfordpublicschools.org/dolan-middle-school/students/pages/teaching-and-learning-resources',
-'https://www.stamfordpublicschools.org/dolan-middle-school/student-support-services',
-'https://www.stamfordpublicschools.org/dolan-middle-school/information/pages/student-handbook',
-'https://www.stamfordpublicschools.org/dolan-middle-school/information/pages/after-school-program',
-'https://www.stamfordpublicschools.org/dolan-middle-school/athletics',
-'https://www.stamfordpublicschools.org/dolan-middle-school/school-based-health-center',
-'https://www.stamfordpublicschools.org/dolan-middle-school/information/pages/photo-galleries-videos',
-'https://www.stamfordpublicschools.org/dolan-middle-school/student-support-services',
+		'https://www.stamfordpublicschools.org/rippowam-middle-school/about-us/pages/administration',
+'https://docs.google.com/spreadsheets/d/1R9nCQ94A-YkTFMBdSZo9ZI4gxlPFhARPI2COwm_M2vM/edit?usp=sharing',
+'https://www.stamfordpublicschools.org/rippowam-middle-school/about-us/pages/mission-vision-and-beliefs',
+'https://www.stamfordpublicschools.org/district/public-affairs/files/rippowam-middle-school-2021',
+'https://www.stamfordpublicschools.org/rippowam-middle-school/about-us/pages/general-information',
+'https://www.stamfordpublicschools.org/rippowam-middle-school/students/pages/teaching-and-learning-resources',
+'https://www.stamfordpublicschools.org/rippowam-middle-school/support-services/pages/technical-support',
+'https://www.stamfordpublicschools.org/rippowam-middle-school/support-services/pages/health-center',
+'https://www.stamfordpublicschools.org/rippowam-middle-school/support-services',
 'https://www.stamfordpublicschools.org/stamford-high-school/school-counseling/pages/naviance-student',
 'https://www.stamfordpublicschools.org/distance-teaching-learning-resources/high-school/pages/hs-opportunities-stamford-8th-graders',
 'https://drive.google.com/file/d/1L-lWRvgSHAqCE6ZqkAMPk7pEEyvtTSmp/view',
 'https://www.stamfordpublicschools.org/district/curriculum-instruction/pages/high-school-program-studies',
 'https://www.stamfordpublicschools.org/turn-river-middle-school/parents/webforms/child-illness',
-'https://www.stamfordpublicschools.org/dolan-middle-school/information/pages/forms-permission-slips',
-'https://dolan-pfo.square.site',
+'https://sites.google.com/stamfordpublicschools.org/rippowammiddleschool/home',
 	]
 	mainfolder = 'stamfordpublicschools'
-	school_name = 'dolan_middle'
+	school_name = 'ripowam_middle'
 	filepath = Path(f'../f_web_interface/static/files/{mainfolder}')
 	filepath.mkdir(parents=True, exist_ok=True)
 
